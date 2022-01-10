88f17069c636dfd8ed3a0199f916267e301e8fb7
Melek <maibalonn@gmail.com>
2022-01-10 21:24:47 +0300

    ..

--- blog3/blog3/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class Blog3Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog3'
-    
-    def ready(self):
-        import blog3.signals
