e0c0d2f58bc24d88b108161788dcb80250115559
Melek <maibalonn@gmail.com>
2022-01-11 17:31:17 +0300

    templates created

--- education_blog/home/urls.py
+++ education_blog/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,about,teacher
 urlpatterns = [
     path('',home,name='home' ),
+    path('about/',about,name='about' ),
+    path('teacher/',teacher,name='teacher' ),
     
 ]
--- education_blog/home/views.py
+++ education_blog/home/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>welcome visdor</h1>')
+    return render(request,'home/index.html')
+
+def about(request):
+    return render(request,'home/about.html')
+def teacher(request):
+    return render(request,'home/teacher.html')
