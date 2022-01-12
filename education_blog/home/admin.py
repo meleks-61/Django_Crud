8eb461d0e594ca105f43ba6e525f75005642e356
Melek <maibalonn@gmail.com>
2022-01-12 14:00:50 +0300

    branch model created

--- education_blog/home/admin.py
+++ education_blog/home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact,Branch,Teacher
 
 class ContactAdmin(admin.ModelAdmin):
     list_display=(
@@ -11,9 +11,29 @@ class ContactAdmin(admin.ModelAdmin):
     )
     list_filter=("name",)
     search_fields=("name__startswith",)
+    
+class TeacherAdmin(admin.ModelAdmin):
+    list_display=(
+        "name",
+        "email",
+        "department",
+        "created_date"
+    )
+    list_filter=("name",)
+    search_fields=("name__startswith",)
+    
+class BranchAdmin(admin.ModelAdmin):
+    list_display=(
+        "branch",
+        "created_date"
+    )
+    list_filter=("branch",)
+    search_fields=("branch__startswith",)
 
 
 
 # Register your models here.
 
 admin.site.register(Contact,ContactAdmin)
+admin.site.register(Branch,BranchAdmin)
+admin.site.register(Teacher,TeacherAdmin)
--- education_blog/home/models.py
+++ education_blog/home/models.py
@@ -1,3 +1,4 @@
+from types import MappingProxyType
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,27 @@ class Contact(models.Model):
     email=models.EmailField(max_length=100)
     message=models.TextField()
     created_date=models.DateTimeField(auto_now_add=True,)
+    
+    def __str__(self):
+        return self.name
+class Branch(models.Model):
+    branch=models.CharField(max_length=150)
+    created_date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.branch
+    
+    
+    
+    
+    
+class Teacher(models.Model):
+    name=models.CharField(max_length=150)
+    email=models.EmailField(max_length=150)
+    department=models.ForeignKey(Branch,on_delete=models.CASCADE)#onetomany(foreignkey) ilişki-istediğimiz zaman branch ekleyip ordan seçim yapabilmek için brunch modeli tanımlayıp buraya relation ile bağlamlıyız.Eğer chociefield yaparsak daha sonra branch eklemek için kodu değiştirmemiz gerekebilir
+    image=models.ImageField(upload_to="teacher",default="default.png")
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
