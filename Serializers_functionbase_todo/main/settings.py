13e683052e6e3d6e26ad2f2b5b097286afe05e12
Melek <maibalonn@gmail.com>
2022-01-19 15:12:41 +0300

    restframework added

--- Serializers_functionbase_todo/main/settings.py
+++ Serializers_functionbase_todo/main/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     #myApps,
     'apiTodo',
     #ThirdpartyApp,
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
