5925e68ac623e550db9986f8e2f2413c82d3a109
Melek <maibalonn@gmail.com>
2022-01-19 15:12:24 +0300

    rest framework set up

--- Serializers_functionbase_todo/apiTodo/admin.py
+++ Serializers_functionbase_todo/apiTodo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display=[
+        "task",
+        "description",
+        "priority",
+        "done",
+    
+    ]#admin dashboardda Todo modelinde gösterilecekler
+
+
+
+admin.site.register(Todo,TodoAdmin)
--- Serializers_functionbase_todo/apiTodo/models.py
+++ Serializers_functionbase_todo/apiTodo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    task=models.CharField(max_length=50)
+    description=models.TextField(blank=True,null=True)
+    TITLE=(
+        ('H','High'),
+        ('M','Medium'),
+        ('L','Low'),
+        
+        
+        
+    )
+    priority = models.CharField(max_length=50,choices=TITLE,default="L")
+    done=models.BooleanField(default=False)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.task
